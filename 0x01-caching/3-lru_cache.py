ad5233b31265a550920f7909f660950d296cf7f4
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-10-30 17:43:08 +0100

    update

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -14,11 +14,11 @@ class LRUCache(BaseCaching):
     '''
 
     def __init__(self):
-            """Initializes the cache.
-            """
-            super().__init__()
-            self.cache_data = OrderedDict()
-    
+        """Initializes the cache.
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
     def put(self, key, item):
         """Adds an item in the cache.
         """
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -14,29 +14,28 @@ class MRUCache(BaseCaching):
     '''
 
     def __init__(self):
+        """Initializes the cache.
+        """
         super().__init__()
-        self.usedKey = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        '''
-        Add an item to the cache with a given key.
-        '''
-        if key is not None and item is not None:
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
             self.cache_data[key] = item
-            if key not in self.usedKeys:
-                self.usedKeys.append(key)
-            else:
-                self.usedKeys.append(
-                    self.usedKeys.pop(self.usedKeys.index(key)))
-            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
-                discard = self.usedKeys.pop(-2)
-                del self.cache_data[discard]
-                print('DISCARD: {:s}'.format(discard))
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        '''
-        Retrieve an item from the cache with a given key.
-        '''
-        if key is not None and key in self.cache_data.keys():
-            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
         return self.cache_data.get(key, None)
