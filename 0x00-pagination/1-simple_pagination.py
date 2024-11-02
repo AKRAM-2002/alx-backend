d01b9c8238c1442ed205c6e0b79aa578e800ac08
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-11-02 01:09:03 +0100

    update

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,11 +40,10 @@ class Server:
         '''
         Returns a paginated dataset of popular baby names.
         '''
-        assert isinstance(page, type(page_size)) == int
-        assert page > 0 and page_size > 0
+        assert isinstance(page, int) and page > 0, "Page must be a positive integer."
+        assert isinstance(page_size, int) and page_size > 0, "Page size must be a positive integer."
 
         start, end = index_range(page, page_size)
         data = self.dataset()
-        print(start, end)
 
         return data[start:end] if start < len(data) else []
