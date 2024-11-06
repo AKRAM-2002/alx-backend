78782a966d49aea683a6c865d07a04b30778dcfd
AKRAM-2002 <Akrambusiness6@gmail.com>
2024-11-06 20:29:48 +0100

    update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 '''
-0. 2. Get locale from request Module
+3. Parametrize templates Module
 '''
 from flask import Flask, render_template, request
-from flask_babel import Babel
-
-app = Flask(__name__)
+from flask_babel import Babel, _, get_locale
 
 
 class Config():
@@ -13,20 +11,20 @@ class Config():
     Configuration class
     '''
     LANGUAGES = ["en", "fr"]
-    DEFAULT_LOCALE = "en"
+    DEFAULT_LOCALE = "fr"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale():
-    '''
-    Get locale
-    '''
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+def get_locale() -> str:
+    """Retrieves the locale for a web page.
+    """
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route('/')
@@ -34,7 +32,7 @@ def index():
     '''
     Index route
     '''
-    return render_template('0-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
